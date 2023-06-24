344725886f6c4e1a32f04cb06f34b2d33dfdcd0b
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-06-24 18:36:56 +0530

    Add files via upload

--- linked_list_q1.java
+++ linked_list_q1.java
@@ -107,7 +107,9 @@ void  delete(int i) {
 		return ;
 	}if(i==0) {
 //		removeFirst();return;
-		head=head.next;return;
+		head=head.next;
+		size--;
+		return;
 	}
 
 	size--;
@@ -160,6 +162,35 @@ node rev(node head) {
 		}
 		System.out.println("Regestration no : "+current.rn+" & Marks obtained : "+current.mark);
 	}
+	boolean palendrom(node head) {
+		if (head==null) {
+			return true;
+		}
+		node mid=middle(head);
+		node last=rev(mid.next);
+		node current =head;
+		while(last!=null) {
+			if(last.rn!=current.rn) {
+				return false;
+			}
+			last=last.next;
+			current =current.next;
+		}
+		return true;
+	}
+	static node middle( node head) {
+		node fast =head;
+		node slow =head;
+		while(fast!=null&&fast.next!=null)
+	{
+			fast=fast.next.next;
+			slow=slow.next;
+			}
+		System.out.println(slow.rn);
+		return slow;
+	}
+	
+	
 }
 
 public class linked_list_q1 {
@@ -168,20 +199,23 @@ public class linked_list_q1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 ll a=new ll();
-a.add_first_s(1, 100);
+a.add_first_s(2, 100);
 a.add_first_s(2, 97);
 a.add_first_s(6, 88);
-a.add_last_s(7, 48);
-a.add(3, 40, 2);
+a.add_last_s(6, 48);
+a.add(2, 40, 2);
 //a.removeFirst();
 //a.removeLast();
 a.display();
 a.getsize();
 //a.get(2);
 //a.head=a.rev(a.head);
-a.delete(0);
+//a.delete(0);
 a.display();
 a.getsize();
+//a.head=a.middle(a.head);
+//System.out.println("middle = "+a.middle(a.head));
+	System.out.println(a.palendrom(a.head));
 	}
 
 }
